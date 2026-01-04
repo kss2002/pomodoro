acce056ae1a67d96ec5b3cdfae989086ec3635b9
kss2002 <sc00310@naver.com>
2026-01-04 13:20:37 +0900

    index에 훅 추가

--- src/layout/Index.tsx
+++ src/layout/Index.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { Btn } from './Btn';
+import { useTitle } from '@/hooks/useTitle';
 
 export const Index = () => {
+  useTitle();
+
   return (
     <>
       <Btn />
